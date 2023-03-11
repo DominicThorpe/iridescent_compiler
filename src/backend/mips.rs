20bc0054fa115f65a1261359f343a21a9bb0c429
thorped1 <dompthorpe123@gmail.com>
2023-03-11 09:46:14 +0000

    Issue #49; Added long division
    Bugfixed long multiplication

--- src/backend/mips.rs
+++ src/backend/mips.rs
@@ -44,6 +44,37 @@ fn get_frame_size(function_id:&str, symbol_table:&SymbolTable) -> u64 {
 }
 
 
+fn get_divintlong() -> Vec<String> {
+    vec![
+        "\n__divint64:".to_owned(),
+        "\tli $t4, 0".to_owned(),
+        "\tli $t5, 0".to_owned(),
+        "\tseq $t1, $a0, $zero".to_owned(),
+        "\tseq $t2, $a1, $zero".to_owned(),
+        "\tand $t1, $t1, $t2".to_owned(),
+        "\tbnez $t1, __divint64_end".to_owned(),
+        "\tmove $t0, $a0".to_owned(),
+        "\tmove $t1, $a1".to_owned(),
+        "\tmove $t2, $a2".to_owned(),
+        "\tmove $t3, $a3".to_owned(),
+        "\n__divint64_loop:".to_owned(),
+        "\tsubu $t0, $t0, $t2".to_owned(),
+        "\tsubu $t1, $t1, $t3".to_owned(),
+        "\taddiu $t5, $t5, 1".to_owned(),
+        "\tsltiu $t7, $t5, 1".to_owned(),
+        "\taddu $t4, $t4, $t7".to_owned(),
+        "\tsleu $t6, $t2, $t0".to_owned(),
+        "\tsleu $t7, $t3, $t1".to_owned(),
+        "\tand $t6, $t6, $t7".to_owned(),
+        "\tbnez $t6, __divint64_loop".to_owned(),
+        "\n__divint64_end:".to_owned(),
+        "\tmove $a0, $t5".to_owned(),
+        "\tmove $a1, $t0".to_owned(),
+        "\tjr $ra\n\n".to_owned(),
+    ]
+}
+
+
 /**
  * Generates the final MIPS assembly code that can then be compiled to native binary using a separate tool.
  */
@@ -56,6 +87,7 @@ pub fn generate_mips(intermediate_code:Vec<IntermediateInstr>, symbol_table:Symb
     let mut stack_types:Vec<Type> = vec![];
 
     mips_instrs.push("j main # start program execution\n\n".to_owned());
+    mips_instrs.append(&mut get_divintlong());
 
     for instr in intermediate_code {
         match instr {
@@ -144,8 +176,8 @@ pub fn generate_mips(intermediate_code:Vec<IntermediateInstr>, symbol_table:Symb
                         stack_types.push(Type::Long);
 
                         let offset = stack_id_offset_map.get(&id).unwrap();
-                        mips_instrs.push(format!("\tlw $t0, -{}($sp) # load long", offset - 4));
-                        mips_instrs.push(format!("\tlw $t1, -{}($sp)", offset));
+                        mips_instrs.push(format!("\tlw $t0, -{}($sp) # load long", offset));
+                        mips_instrs.push(format!("\tlw $t1, -{}($sp)", offset - 4));
                         mips_instrs.push(format!("\tsw $t0, {}($sp)", current_stack_offset));
                         mips_instrs.push(format!("\tsw $t1, {}($sp)\n", current_stack_offset - 4));
                     },
@@ -260,10 +292,10 @@ pub fn generate_mips(intermediate_code:Vec<IntermediateInstr>, symbol_table:Symb
                     },
 
                     Type::Long => {
-                        mips_instrs.push(format!("\tlw $t1, {}($sp) # multiply long", current_stack_offset));
-                        mips_instrs.push(format!("\tlw $t0, {}($sp)", current_stack_offset - 4));
-                        mips_instrs.push(format!("\tlw $t3, {}($sp)", current_stack_offset - 8));
-                        mips_instrs.push(format!("\tlw $t2, {}($sp)", current_stack_offset - 12));
+                        mips_instrs.push(format!("\tlw $t0, {}($sp) # multiply long", current_stack_offset));
+                        mips_instrs.push(format!("\tlw $t1, {}($sp)", current_stack_offset - 4));
+                        mips_instrs.push(format!("\tlw $t2, {}($sp)", current_stack_offset - 8));
+                        mips_instrs.push(format!("\tlw $t3, {}($sp)", current_stack_offset - 12));
 
                         mips_instrs.push(format!("\tmult $t0, $t2"));
                         mips_instrs.push(format!("\tmflo $t4"));
@@ -275,8 +307,8 @@ pub fn generate_mips(intermediate_code:Vec<IntermediateInstr>, symbol_table:Symb
                         mips_instrs.push(format!("\tmfhi $t7"));
                         mips_instrs.push(format!("\tadd $t5, $t7, $s1"));
 
-                        mips_instrs.push(format!("\tsw $t5, {}($sp)", current_stack_offset - 8));
-                        mips_instrs.push(format!("\tsw $t4, {}($sp)\n", current_stack_offset - 12));
+                        mips_instrs.push(format!("\tsw $t4, {}($sp)", current_stack_offset - 8));
+                        mips_instrs.push(format!("\tsw $t5, {}($sp)\n", current_stack_offset - 12));
 
                         current_stack_offset -= 8;
 
@@ -293,7 +325,7 @@ pub fn generate_mips(intermediate_code:Vec<IntermediateInstr>, symbol_table:Symb
                 let mult_type = stack_types.pop().unwrap();
                 match mult_type {
                     Type::Integer => {
-                        mips_instrs.push(format!("\tlw $t0, {}($sp)", current_stack_offset));
+                        mips_instrs.push(format!("\tlw $t0, {}($sp) # divide int", current_stack_offset));
                         mips_instrs.push(format!("\tlw $t2, {}($sp)", current_stack_offset - 4));
                         mips_instrs.push(format!("\tdiv $t0, $t2, $t0"));
                         mips_instrs.push(format!("\tsw $t0, {}($sp)\n", current_stack_offset - 4));
@@ -301,6 +333,27 @@ pub fn generate_mips(intermediate_code:Vec<IntermediateInstr>, symbol_table:Symb
                         current_stack_offset -= 4;
                         stack_types.pop();
                         stack_types.push(Type::Integer);
+                    },
+
+                    Type::Long => {
+                        mips_instrs.push(format!("\tlw $a0, {}($sp)", current_stack_offset - 8));
+                        mips_instrs.push(format!("\tlw $a1, {}($sp)", current_stack_offset - 12));
+                        mips_instrs.push(format!("\tlw $a2, {}($sp) # divide long", current_stack_offset));
+                        mips_instrs.push(format!("\tlw $a3, {}($sp)", current_stack_offset - 4));
+                        
+                        mips_instrs.push(format!("\tjal __divint64"));
+
+                        mips_instrs.push(format!("\tmove $t0, $a0"));
+                        mips_instrs.push(format!("\tmove $t1, $a1"));
+
+                        mips_instrs.push(format!("\tsw $t0, {}($sp)", current_stack_offset - 8));
+                        mips_instrs.push(format!("\tsw $t1, {}($sp)\n", current_stack_offset - 12));
+
+                        current_stack_offset -= 8;
+
+                        stack_types.pop();
+                        stack_types.pop();
+                        stack_types.push(Type::Long);
                     },
 
                     _ => todo!()
@@ -458,7 +511,8 @@ pub fn generate_mips(intermediate_code:Vec<IntermediateInstr>, symbol_table:Symb
         }
     }
 
-    mips_instrs.push("\n\tli $v0, 10 # halt syscall".to_owned());
+    mips_instrs.push("\nend:".to_owned());
+    mips_instrs.push("\tli $v0, 10 # halt syscall".to_owned());
     mips_instrs.push("\tsyscall".to_owned());
 
     file.write(mips_instrs.join("\n").as_bytes()).expect("Could not write target code to file");
